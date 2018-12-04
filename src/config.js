0cade4c3877a1aefe081759cbcc1689242853775
Andrea Siede <andrea.siede@gmail.com>
2018-12-03 21:26:57 -0800

    updated api link

--- src/config.js
+++ src/config.js
@@ -4,6 +4,5 @@ module.exports = {
   // PORT: process.env.PORT || 8080,
   // other stuff
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8080" ||
-    "https://dry-ravine-94147.herokuapp.com/"
+    "https://dry-ravine-94147.herokuapp.com"
 };
